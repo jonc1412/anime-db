af58f144a159cc979e4b52ab14e23c6174c93e92
jonc1412 <jon.c2436@gmail.com>
2025-02-11 21:39:11 +0900

    check_ongoing function for updating anime with status=ongoing

--- scripts/update_anime.py
+++ scripts/update_anime.py
@@ -14,24 +14,43 @@ DB_PASSWORD = os.getenv("PG_PASSWORD")
 DB_HOST = os.getenv("PG_HOST")
 DB_PORT = os.getenv("PG_PORT")
 
-conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
-cur = conn.cursor()
+# Step 1: Check if any of the 'RELEASING' animes are updated (status, episodes)
+def check_ongoing(conn, api_data):
+    cur = conn.cursor()
 
-cur.execute("""
-        SELECT anime_id, episodes, status, average_score, popularity
-        FROM anime a
-        JOIN Ratings r USING (anime_id)
-        """)
-existing_data = {row[0]: {
-    'episodes': row[1],
-    'status': row[2],
-    'average_score': row[3],
-    'popularity': row[4]
-} for row in cur.fetchall()}
+    cur.execute("""
+        SELECT anime_id, episodes, status
+        FROM anime
+    """)
+    ongoing_anime = {row[0]: {
+        'episodes': row[1],
+        'status': row[2]
+    } for row in cur.fetchall()}
 
-for anime in api_data:
-    anime_id = anime['id']
+    updates = []
+    for anime in api_data:
+        anime_id = anime['id']
 
-    if anime in existing_data:
-        if existing_data[anime_id]['status'] == 'RELEASING':
-            
+        if anime_id in ongoing_anime:
+            old_status = ongoing_anime[anime_id]['status']
+            old_episodes = ongoing_anime[anime_id]['episodes']
+
+            new_status = anime['status']
+            new_episodes = anime['episodes']
+
+            if old_status != new_status or old_episodes != new_episodes:
+                updates.append((new_episodes, new_status, anime_id))
+    
+    if updates:
+        cur.executemany("""
+            UPDATE anime
+            SET episodes = %s, status = %s
+            WHERE anime_id = %s
+        """, updates)
+
+        conn.commit()
+        print('Successfully updated animes with the status (RELEASING)')
+    else:
+        print('No changes were made')
+
+    cur.close()
